9a25862a735b28bacb053f8aabcf70107b084e53
Taniagm102002 <72842637+Taniagm102002@users.noreply.github.com>
2020-10-14 15:26:51 -0700

    Update script.py

--- script.py
+++ script.py
@@ -19,3 +19,25 @@ def summarize_contens(filename):
                 print("ID:",avance.id)
                 
 summarize_contents(filename)
+
+from Bio.Seq import Seq
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+import os
+def summarize_contents(filename):
+	all_records = []
+	record = list(SeqIO.parse(filename,"genbank"))
+	print("Path: ",os.path.dirname(filename))
+	print("Num_record = %i records" %len(record))
+	print("\n")
+	for seq_r in SeqIO.parse(filename,"genbank"):
+		all_records.append(seq_r.name)
+		print("Name: ",seq_r.name)
+		print("ID :",seq_r.id)
+		print("Location:")
+		for seq_features in seq_r.features :
+			print('Star: %d, Stop: %d' %(int(seq_feature.location.start),int(seq_feature.location.end)))
+		
+	
+sunnarize_contents(filename)
