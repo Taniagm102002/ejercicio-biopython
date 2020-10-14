58f129684b4f559627a2997da2330298e21e6ebd
Taniagm102002 <72842637+Taniagm102002@users.noreply.github.com>
2020-10-14 14:01:10 -0700

    Update script.py

--- script.py
+++ script.py
@@ -6,3 +6,16 @@ def summarize_contens(filename):
     print(record)
  summarize_contents(filename)
 
+                                      
+    def summarize_contents(filename):
+       avance = SeqIO.read(filename, "genbank")
+        print("Name: ", avance.name)
+        import os
+        print ("Path: ", os.path.dirname(filename))
+       avances = list(SeqIO.parse(filename, "genbank"))
+        print("num_avances = %i avances" % len(avance))
+
+        for seq_record in SeqIO.parse(filename,"genbank"):
+                print("ID:",avance.id)
+                
+summarize_contents(filename)
